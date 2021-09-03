e6cd312293d9ea498236c264a030c70ebc251c4f
Francisco Demartino <demartino.francisco@gmail.com>
2021-09-03 02:59:14 -0300

    useful cli tools + apt-file db

--- install/1-cli.sh
+++ install/1-cli.sh
@@ -4,16 +4,20 @@ set -ex
 source /etc/profile
 
 DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends \
+    apt-file \
     caca-utils \
     curl \
     figlet \
     gcc \
     git \
+    htop \
     inetutils-ping \
     iproute2 \
     less \
+    make \
     man \
     nano \
+    net-tools \
     network-manager \
     network-manager-gnome \
     ranger \
@@ -22,3 +26,5 @@ DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends \
     vim \
     wget \
     # done
+
+apt-file update
