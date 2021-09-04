2e3b18144e5efbbd85bb9d51dc790b4d07256f94
Francisco Demartino <demartino.francisco@gmail.com>
2021-09-04 13:51:19 -0300

    cleanup comments

--- install/0-base.sh
+++ install/0-base.sh
@@ -5,15 +5,4 @@ source /etc/profile
 
 apt update
 
-DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends linux-image-amd64 live-boot systemd-sysv 
-## grub2-common grub-pc
-
-## echo "LABEL=DEBUSB / ext4 defaults 0 1" > /etc/fstab
-## passwd root
-
-##  grub-install \
-##      --target=i386-pc \
-##      --boot-directory=/boot \
-##      --force-file-id \
-##      --skip-fs-probe \
-##      --recheck /dev/loop0
+DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends linux-image-amd64 live-boot systemd-sysv
