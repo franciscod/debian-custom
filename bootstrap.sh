8e05be0bc94f7e0495a1377c7e75ab0783eeb76c
Francisco Demartino <demartino.francisco@gmail.com>
2021-09-04 14:43:20 -0300

    remove broken line (bin/ is gone)

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,6 @@ ROOT=out/debian
 
 sudo debootstrap --arch=amd64 --variant=minbase bullseye $ROOT
 sudo cp -r install/ $ROOT/root/install
-sudo cp -r bin/ $ROOT/root/install
 
 echo "debian-custom" | sudo tee $ROOT/etc/hostname
 echo "127.0.0.1 localhost debian-custom" | sudo tee $ROOT/etc/hosts
