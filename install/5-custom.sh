e46645a5b6fb84a0a5b52a6c44c5d3329cd31a8b
Francisco Demartino <fdemartino@huergo.edu.ar>
2021-10-26 21:27:19 -0300

    add virtualbox via fasttrack

--- install/5-custom.sh
+++ install/5-custom.sh
@@ -7,3 +7,13 @@ DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends \
     cmatrix \
     tint \
     # done
+
+# fasttrack for virtualbox
+apt install fasttrack-archive-keyring
+
+echo "" >> /etc/apt/sources.list
+echo "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib" >> /etc/apt/soruces.list
+echo "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib" >> /etc/apt/sources.list
+
+apt update
+apt install virtualbox virtualbox-ext-pack
