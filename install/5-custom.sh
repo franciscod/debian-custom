9b2b36b819a6db085bf2e1ceae7e5ed887306201
Francisco Demartino <demartino.francisco@gmail.com>
2021-10-28 16:43:05 -0300

    typo

--- install/5-custom.sh
+++ install/5-custom.sh
@@ -12,7 +12,7 @@ DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends \
 apt install fasttrack-archive-keyring
 
 echo "" >> /etc/apt/sources.list
-echo "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib" >> /etc/apt/soruces.list
+echo "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib" >> /etc/apt/sources.list
 echo "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib" >> /etc/apt/sources.list
 
 apt update
