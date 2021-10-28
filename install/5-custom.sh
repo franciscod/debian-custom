f5bbea7cda628da4abb7873536ed8931540b4a24
Francisco Demartino <demartino.francisco@gmail.com>
2021-10-28 17:46:35 -0300

    remove virtualbox packages
    virtualbox requires an eula that apt won't accept
    
    users can download + install them manually after booting with:
    
            sudo apt install virtualbox virtualbox-ext-pack

--- install/5-custom.sh
+++ install/5-custom.sh
@@ -14,6 +14,3 @@ DEBIAN_FRONTEND=noninteractive apt install -y fasttrack-archive-keyring
 echo "" >> /etc/apt/sources.list
 echo "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib" >> /etc/apt/sources.list
 echo "deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib" >> /etc/apt/sources.list
-
-apt update
-DEBIAN_FRONTEND=noninteractive apt install -y virtualbox virtualbox-ext-pack
