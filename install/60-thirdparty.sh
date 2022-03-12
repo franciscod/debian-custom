948ab06cdc26bd3580dae8b8d4bebe65d20baded
Francisco Demartino <demartino.francisco@gmail.com>
2022-03-12 15:02:17 -0300

    extract godot first

--- install/60-thirdparty.sh
+++ install/60-thirdparty.sh
@@ -3,7 +3,9 @@ set -ex
 
 source /etc/profile
 
-SUBL=$(pwd)/thirdparty/dl/sublime_text_4.tar.xz
+DL=$(pwd)/thirdparty/dl
+
+SUBL=$DL/sublime_text_4.tar.xz
 pushd /opt
 tar xf $SUBL
 popd
@@ -11,10 +13,17 @@ pushd /usr/local/bin
 ln -s /opt/sublime_text/sublime_text
 popd
 
-LIBRESPRITE=$(pwd)/thirdparty/dl/LibreSprite.AppImage
+LIBRESPRITE=$DL/LibreSprite.AppImage
 cp $LIBRESPRITE /usr/local/bin/libresprite
 chmod +x /usr/local/bin/libresprite
 
-GODOT=$(pwd)/thirdparty/dl/Godot_v3.4.3-stable_x11.64.zip
-cp $GODOT /usr/local/bin/godot
-chmod +x /usr/local/bin/godot
+GODOTFILE=Godot_v3.4.3-stable_x11.64
+GODOTZIP=$DL/$GODOTFILE.zip
+GODOTDIR=/opt/godot
+mkdir -p $GODOTDIR
+pushd $GODOTDIR
+unzip $GODOTZIP
+popd
+pushd /usr/local/bin
+ln -s $GODOTDIR/$GODOTFILE godot
+popd
