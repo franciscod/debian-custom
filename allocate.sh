24c8fe11e5ab572a9818e501f5dc9a2f533fddef
Francisco Demartino <demartino.francisco@gmail.com>
2022-03-21 14:16:46 -0300

    bump size

--- allocate.sh
+++ allocate.sh
@@ -3,8 +3,9 @@ set -ex
 
 IMG=out/debian.img
 
-fallocate -l 3500M $IMG
-echo -e "o\nn\np\n1\n\n\nw" | sudo fdisk $IMG
+fallocate -l 4000M $IMG
+echo -e "o\n" | sudo fdisk $IMG
+echo -e "n\np\n1\n\n\nw" | sudo fdisk $IMG
 echo -e "a\nw\n" | sudo fdisk $IMG
 
 sudo losetup --partscan --show --find $IMG > /tmp/debian-custom-loop
