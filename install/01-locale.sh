e4901e149299a9fe11e805cba4033ab073b9e83c
Francisco Demartino <demartino.francisco@gmail.com>
2023-04-21 08:47:27 -0300

    es_AR locale

--- install/01-locale.sh
+++ install/01-locale.sh
@@ -8,8 +8,9 @@ DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends \
     # done
 
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "es_AR.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/default/locale
+echo "LANG=es_AR.UTF-8" > /etc/default/locale
 
 # remove default utc
 rm /etc/localtime
